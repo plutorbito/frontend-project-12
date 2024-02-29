746222f087cdffe8d23a8814dcca039021bdd2e1
plutorbito <plutorbito@gmail.com>
2024-02-29 22:37:52 +0100

    rollbar added

--- frontend-project-12-react-app/src/App.js
+++ frontend-project-12-react-app/src/App.js
@@ -16,6 +16,14 @@ import useAuth from './hooks';
 import SignupPage from './components/SignupPage';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Provider, ErrorBoundary } from '@rollbar/react';
+
+const rollbarConfig = {
+  accessToken: process.env.REACT_APP_ROLLBAR_ACCESSTOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  environment: 'production',
+};
 
 const AuthProvider = ({ children }) => {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -47,36 +55,40 @@ const ChatRoute = ({ children }) => {
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <div className="d-flex flex-column h-100">
-          <NavbarElement />
-          <Routes>
-            <Route path={ROUTERS.loginPage} element={<LoginPage />} />
-            <Route path={ROUTERS.other} element={<NotFoundPage />} />
-            <Route
-              path={ROUTERS.chatPage}
-              element={
-                <ChatRoute>
-                  <ChatPage />
-                </ChatRoute>
-              }
+      <Provider config={rollbarConfig}>
+        <ErrorBoundary>
+          <BrowserRouter>
+            <div className="d-flex flex-column h-100">
+              <NavbarElement />
+              <Routes>
+                <Route path={ROUTERS.loginPage} element={<LoginPage />} />
+                <Route path={ROUTERS.other} element={<NotFoundPage />} />
+                <Route
+                  path={ROUTERS.chatPage}
+                  element={
+                    <ChatRoute>
+                      <ChatPage />
+                    </ChatRoute>
+                  }
+                />
+                <Route path={ROUTERS.signupPage} element={<SignupPage />} />
+              </Routes>
+            </div>
+            <ToastContainer
+              position="top-right"
+              autoClose={3000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="light"
             />
-            <Route path={ROUTERS.signupPage} element={<SignupPage />} />
-          </Routes>
-        </div>
-        <ToastContainer
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-        />
-      </BrowserRouter>
+          </BrowserRouter>
+        </ErrorBoundary>
+      </Provider>
     </AuthProvider>
   );
 }
